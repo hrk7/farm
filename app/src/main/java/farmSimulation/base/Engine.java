e78900e9cdc49f5195db9c1df7d9d30db7a3477f
Yaroslav <ta1son5951@gmai.com>
2026-05-18 15:17:52 +0200

    engine + ticks

---
+++ app/src/main/java/farmSimulation/base/Engine.java
@@ -0,0 +1,88 @@
+package farmSimulation.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Engine {
+    private List<Potato> potatoes = new ArrayList<>();
+    private List<Beetle> beetles = new ArrayList<>();
+    private List<Fox> foxes = new ArrayList<>();
+    private List<Chicken> chickens = new ArrayList<>();
+    private List<Farmer> farmers = new ArrayList<>();
+
+    private int currentTick = 0;
+
+    public void addPotato(Potato p) {
+        potatoes.add(potato);
+    }
+
+    public void addBeetle(Beetle b) {
+        beetles.add(beetle);
+    }
+
+    public void addFox(Fox f) {
+        foxes.add(fox);
+    }
+
+    public void addChicken(Chicken c) {
+        chickens.add(chicken);
+    }
+
+    public void addFarmer(Farmer f) {
+        farmers.add(farmer);
+    }
+
+    public void nextTick() {
+        currentTick++;
+
+        runGrowthAndPestPhase(); // фаза роста картофеля и атаки жуков
+        runThreatPhase(); // фаза атаки лис
+        runChickenResponsePhase(); // фаза реакции чикенов
+        runFarmerPhase(); // фаза реакции фермера
+
+        cleanUpDeadEntities(); // удаление мертвых субъектов
+    }
+
+    private void runGrowthAndPestPhase() {
+        for (Potato potato : potatoes) {
+            potato.grow();
+        }
+
+        for (ColoradoBeetle beetle : beetles) {
+            if (beetle.isAlive()) {
+                // логика поедания жуком картофеля beetle.eat(targetPotato)
+            }
+        }
+    }
+
+    private void runThreatPhase() {
+        for (Fox fox : foxes) {
+            // логика поиска курицы и атаки в случае нахождения fox.hunt(targetChicken);
+        }
+    }
+
+    private void runChickenResponsePhase() {
+        for (Chicken chicken : chickens) {
+            if (chicken.isAlive()) {
+                // логика взаимодействия куры с жуками или картофелем chicken.interact(targetBeetle, targetPotato);
+                // +логика побега если рядом лиса
+            }
+        }
+    }
+
+    private void runFarmerPhase() {
+        for (Farmer farmer : farmers) {
+            // логика защиты farmer.protect(...) и сбора урожая farmer.harvest(...)
+        }
+    }
+
+    private void cleanUpDeadEntities() {
+        beetles.removeIf(beetle -> !beetle.isAlive());
+        chickens.removeIf(chicken -> !chicken.isAlive());
+        potatoes.removeIf(potato -> potato.getMass() <= 0);
+    }
+
+    public int getCurrentTick() {
+        return currentTick;
+    }
+}
